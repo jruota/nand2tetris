cfb6755c06aeebc8561bb71f4584bf60d8bac337
Jarosław Kołosowski <jarokolosowski@gmail.com>
2024-01-13 14:42:04 +0100

    Project 6 - Hack assembler
    Reads single command line argument and opens the input file.

---
+++ nand2tetris/projects/06/hack_assembler.py
@@ -0,0 +1,32 @@
+# Quote from the book:
+#    This version of the assembler assumes that the source assembly code is
+#    error-free. Error checking, reporting, and handling can be added to later
+#    versions of the assembler but are not part of project 6.
+
+import argparse
+
+def hack_assembler():
+    # parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "fp", 
+        help="File or file path to the file containing assembly instructions." +
+             "Must end with '.asm' file extension.")
+    args = parser.parse_args()
+    
+    # test if file ends with '.asm' file extension
+    if (not args.fp.endswith(".asm", -4)):
+        raise Warning("Filename must end in '.asm' file extension.")
+        return
+
+    # open input file containing assembly instructions
+    inputf = open(args.fp, 'r')
+
+    # close files
+    inputf.close()
+    return
+
+################################################################################
+
+if __name__ == "__main__":
+    hack_assembler()
